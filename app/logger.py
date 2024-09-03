b47a9a9564da76c753423753c8c7b79c4a9dde4c
Nilser Tejedo Vega <ntejedov>
2024-09-02 19:05:54 -0500

    Fix logger and add deploy to fly.io

--- app/logger.py
+++ app/logger.py
@@ -5,7 +5,7 @@ def setup_logger():
     logger = logging.getLogger('RSSLogger')
     logger.setLevel(logging.INFO)
     
-    handler = RotatingFileHandler()
+    handler = logging.StreamHandler()
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     
